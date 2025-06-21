46bb399008920b2347844ea432186319a1b9ce5a
Rehan Mulya Alamsyah <mulyarehanalamsyah@gmail.com>
2025-06-21 17:08:58 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\DB;
 use View;
 
 class AppServiceProvider extends ServiceProvider
